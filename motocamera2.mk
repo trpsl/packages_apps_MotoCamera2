3fd28f03962b8ba7fdb081840e5340e4110c3828
trpsl <trpsl755@gmail.com>
2020-03-31 20:56:12 +0200

    add permissions and move to system

--- motocamera2.mk
+++ motocamera2.mk
@@ -1,3 +1,7 @@
 # MotoCamera2
+PRODUCT_COPY_FILES += \
+    packages/apps/MotoCamera2/com.motorola.camera2.xml:system/etc/permissions/com.motorola.camera2.xml \
+    packages/apps/MotoCamera2/privapp-permissions-com.motorola.camera2.xml:system/etc/permissions/privapp-permissions-com.motorola.camera2.xml
+
 PRODUCT_PACKAGES += \
     MotoCamera2
